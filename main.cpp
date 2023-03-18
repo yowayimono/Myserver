2ca4053332d072ca16ab4580d3a96a60eb15ee83
yowayimono <3485000346@qq.com>
2023-03-18 17:28:47 +0800

    httpresquest

--- main.cpp
+++ main.cpp
@@ -1,19 +1,13 @@
 /*
  * @Author: yowayimono 3485000346@qq.com
- * @Date: 2023-03-13 22:16:48
+ * @Date: 2023-03-18 08:00:37
  * @LastEditors: yowayimono 3485000346@qq.com
- * @LastEditTime: 2023-03-13 22:29:29
- * @FilePath: /Myserver/test3.cpp
+ * @LastEditTime: 2023-03-18 08:27:34
+ * @FilePath: /Myserver/main.cpp
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-#include<iostream>
-template<class output>
-void print(output outs)
-{
-    std::cout<<outs<<std::endl;
-}
+#include <iostream>
 int main()
 {
-    print("hello github!");
-    return 0;
+    
 }
