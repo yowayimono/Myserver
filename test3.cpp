a0659e58fdb544c5824c640c410889ec193ab759
yowayimono <3485000346@qq.com>
2023-03-18 17:01:36 +0800

    http slove

---
+++ test3.cpp
@@ -0,0 +1,19 @@
+/*
+ * @Author: yowayimono 3485000346@qq.com
+ * @Date: 2023-03-13 22:16:48
+ * @LastEditors: yowayimono 3485000346@qq.com
+ * @LastEditTime: 2023-03-13 22:29:29
+ * @FilePath: /Myserver/test3.cpp
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+#include<iostream>
+template<class output>
+void print(output outs)
+{
+    std::cout<<outs<<std::endl;
+}
+int main()
+{
+    print("hello github!");
+    return 0;
+}
